502058681643770fcff38f950a83628a41a23917
Shanky0 <shashank.20747@knit.ac.in>
2023-07-23 22:00:53 +0530

    added logout functionality on profile page

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+	try {
+		const response = NextResponse.json(
+			{ message: "Logout successfully", success: true },
+			{ status: 200 }
+		);
+		response.cookies.set("token", "", {
+			httpOnly: true,
+			expires: Date.now(),
+		});
+        return response;
+	} catch (error: any) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
+};
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,36 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const ProfilePage = () => {
-  return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-4xl">ProfilePage</h1>
-    </div>
-  );
+	const router = useRouter();
+	const handleLogout = async () => {
+		try {
+			const res: any = await axios.get("/api/users/logout");
+			if (res.data.success) {
+				alert(res.data.message);
+				router.push("/");
+			} else {
+				alert(res.data.error);
+			}
+		} catch (error: any) {
+			console.log(error.message);
+		}
+	};
+	return (
+		<div className="flex flex-col justify-center items-center min-h-screen">
+			<h1 className="text-4xl">ProfilePage</h1>
+			<hr />
+			<button
+				className="rounded text-white mt-3 px-3 py-1 bg-red-500"
+				onClick={handleLogout}
+			>
+				Logout
+			</button>
+		</div>
+	);
 };
 
 export default ProfilePage;
